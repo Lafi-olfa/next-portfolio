bcc9f3d1689876685648a5f12d4002b757d20548
olfalafi <olfalafi1998@gmail.com>
2025-08-15 21:25:43 +0100

    fix navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,49 +1,27 @@
 'use client';
+import Link from 'next/link';
 import { useState } from 'react';
 import { FaBars } from 'react-icons/fa';
 import { IoClose } from 'react-icons/io5';
-import { MdDarkMode } from "react-icons/md";
-import { CiLight } from "react-icons/ci";
 
 export default function Navbar() {
-  const [isMobile, setIsMobile] = useState(false);
-  const [isDarkMode, setIsDarkmode] = useState(true);
-  function handleClick() {
-    setIsMobile(prev => !prev);
-  }
-    function handleClickMode() {
-    setIsDarkmode(prev => !prev);
-  }
-  return (
-    <div className={`navbar-container ${isDarkMode ? 'dark' : 'light'}`}>
-      <div className="logo">
-        <p>Portfolio</p>
-        {/* <img src="" alt="" srcset="" /> */}
-      </div>
-      <div className="mode btn d-lg-none" onClick={handleClickMode}>
-        {isDarkMode ? <MdDarkMode /> : <CiLight />}
-        </div>
-            <div className="bars btn d-lg-none d-md-none" onClick={handleClick}>
-        {isMobile ? <IoClose /> : <FaBars />}
-        </div>
-        {isMobile && (
-        <div className="navbar-list-mobile d-lg-none d-md-none">
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Projects</li>
-            <li>Contact</li>
-          </ul>
-        </div> )}
+  const [isOpen, setIsOpen] = useState(false);
 
-        <div className="navbar-list d-none d-md-flex">
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Projects</li>
-            <li>Contact</li>
-          </ul>
-        </div>
-    </div>
+  return (
+    <nav className="navbar">
+      <div className="logo">Portfolio</div>
+
+      <div className="menu-icon" onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? <IoClose /> : <FaBars />}
+      </div>
+
+      
+      <ul className={`nav-links ${isOpen ? 'active' : ''}`}>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/about">About</Link></li>
+        <li><Link href="/projects">Projects</Link></li>
+        <li><Link href="/contact">Contact</Link></li>
+      </ul>
+    </nav>
   );
 }
